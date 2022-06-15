9dbc3ce02b4d034ab1aa8b54b0637fe19705287d
Dheniffe de Oliveira <93141575+dheniffe@users.noreply.github.com>
2022-06-15 08:37:16 -0300

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,18 @@
+for (let num = 1; num <= 10; num++) {
+  console.log(num);
+}
+
+let un = 10;
+
+while (un >= 1) {
+  console.log(un);
+  un--;
+}
+
+for (let cen = 1; cen <= 100; cen++) {
+  if (cen % 2 !== 0) console.log(cen);
+}
+
+for (let cen = 1; cen <= 100; cen++) {
+  if (cen % 2 === 0) console.log(cen);
+}

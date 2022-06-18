3863ef62ab41591747bd0b60941a1634d554e32c
Dheniffe de Oliveira <93141575+dheniffe@users.noreply.github.com>
2022-06-18 13:10:40 -0300

    using for and while using for and while to solve all exercises

--- src/index.js
+++ src/index.js
@@ -1,18 +1,56 @@
-for (let num = 1; num <= 10; num++) {
-  console.log(num);
-}
+//Criar um loop que conte de 1 até 10 usando FOR
 
-let un = 10;
+/*for (let dezena = 1; dezena <= 10; dezena++) {
+  console.log(dezena);
+}*/
 
-while (un >= 1) {
-  console.log(un);
-  un--;
-}
+/*let dezena = 1;
 
-for (let cen = 1; cen <= 100; cen++) {
-  if (cen % 2 !== 0) console.log(cen);
-}
+while (dezena <=10) {
+  console.log(dezena);
+  dezena++
+}*/
 
-for (let cen = 1; cen <= 100; cen++) {
-  if (cen % 2 === 0) console.log(cen);
-}
+//Criar um loop que conte de 10 até 1 usando WHILE
+/*let unidade = 10;
+
+while (unidade >= 1) {
+  console.log(unidade);
+  unidade--
+}*/
+
+/*for(let unidade = 10; unidade>=1; unidade--) {
+  console.log(unidade)
+}*/
+
+//Criar um loop que conte todos os números ímpares de 1 até 100
+/*for (let centena = 1; centena <= 100; centena++) {
+  if (centena % 2 !== 0) { 
+    console.log(centena);
+  }
+}*/
+
+/*let centena = 1;
+
+while (centena <= 100) {
+  if(centena % 2 !== 0) {
+    console.log(centena);
+ }
+  ++centena;
+}*/
+
+// Criar um loop que conte todos os números pares de 0 a 100
+/*for (let centena = 1; centena <= 100; centena++) {
+  if (centena % 2 === 0) {
+    console.log(centena);
+  }
+}*/
+
+/*let centena = 1;
+
+while(centena <= 100) {
+  if(centena % 2 === 0) {
+    console.log(centena)
+  }
+  ++centena
+}*/
